32c6d6ccd41360aeba42325717275c187ec7d2ef
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-12-29 18:43:10 -0300

    :wrench: update header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Inicio from '../../Pages/Inicio/Inicio.jsx';
+import Curiosidade from '../../Pages/Curiosidade/Curiosidade.jsx';
+import Galeria from '../../Pages/Galeria/Galeria.jsx';
+import arvoreNatalLogo from '../../assets/arvore-icone-nav.png';
+// import s from './header.module.scss';
 
 export default function Header() {
     return (
         <BrowserRouter>
         <header>
-
+            <section>
+                <img src={arvoreNatalLogo} alt="Imagem de uma logo de arvore de natal com presentes" />
+                <h1>Feliz Natal</h1>
+            </section>
+            <nav>
+                <ul>
+                    <li>
+                        <Link>Inicio</Link>
+                    </li>
+                    <li>
+                        <Link>Curiosidade</Link>
+                    </li>
+                    <li>
+                        <Link>Galeria</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
 
         <Routes>
@@ -13,5 +34,5 @@ export default function Header() {
             <Route path="/Galeria" element={<Galeria/>} />
         </Routes> 
         </BrowserRouter>
-    )
+    );
 }
